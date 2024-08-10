4e0eb850601db57dd86b7e62e2f66eaaac74585b
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-08-10 19:54:13 -0300

    tratando de aplicar css a carousel de sponsors

--- components/SponsorsCarousel.js
+++ components/SponsorsCarousel.js
@@ -2,42 +2,44 @@ import Image from 'next/image';
 
 export const SponsorsCarousel = () => {
     return (
-        <div class="overflow-hidden flex">
-            <div class="flex animate-slide">
-                <Image src={"/sponsor1.png"} alt="Sponsor 1" height={24} width={40} />
-                <img src="sponsor2.png" alt="Sponsor 2" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor3.png" alt="Sponsor 3" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor4.png" alt="Sponsor 4" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor5.png" alt="Sponsor 5" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor6.png" alt="Sponsor 6" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor7.png" alt="Sponsor 7" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor8.png" alt="Sponsor 8" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor9.png" alt="Sponsor 9" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor10.png" alt="Sponsor 10" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor11.png" alt="Sponsor 11" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor12.png" alt="Sponsor 12" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor13.png" alt="Sponsor 13" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor14.png" alt="Sponsor 14" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor15.png" alt="Sponsor 15" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor16.png" alt="Sponsor 16" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor17.png" alt="Sponsor 17" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor1.png" alt="Sponsor 1" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor2.png" alt="Sponsor 2" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor3.png" alt="Sponsor 3" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor4.png" alt="Sponsor 4" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor5.png" alt="Sponsor 5" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor6.png" alt="Sponsor 6" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor7.png" alt="Sponsor 7" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor8.png" alt="Sponsor 8" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor9.png" alt="Sponsor 9" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor10.png" alt="Sponsor 10" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor11.png" alt="Sponsor 11" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor12.png" alt="Sponsor 12" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor13.png" alt="Sponsor 13" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor14.png" alt="Sponsor 14" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor15.png" alt="Sponsor 15" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor16.png" alt="Sponsor 16" class="w-40 h-24 object-contain mr-4" />
-                <img src="sponsor17.png" alt="Sponsor 17" class="w-40 h-24 object-contain mr-4" />
+        <div class="overflow-hidden w-full nowrap bg-gray-600">
+            <div class="flex space-x-4 animate-scroll inline-block">
+                <Image src={"/sponsor1.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor3.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor2.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor4.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor5.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor6.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor7.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor8.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor9.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor10.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor11.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor12.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor13.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor14.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor15.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor16.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor17.png"} alt="Sponsor 1" height={300} width={267} />
+            </div>
+            <div class="flex space-x-4 animate-scroll inline-block">
+                <Image src={"/sponsor1.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor3.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor2.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor4.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor5.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor6.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor7.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor8.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor9.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor10.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor11.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor12.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor13.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor14.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor15.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor16.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor17.png"} alt="Sponsor 1" height={300} width={267} />
             </div>
         </div>
     );
