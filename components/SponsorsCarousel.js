bdbed4459126ca28559910b26a14538ed1717fc4
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-08-15 19:43:10 -0300

    carousel de sponsors ok

--- components/SponsorsCarousel.js
+++ components/SponsorsCarousel.js
@@ -3,10 +3,27 @@ import Image from 'next/image';
 export const SponsorsCarousel = () => {
     return (
         <div class="overflow-hidden w-full nowrap bg-gray-600">
-            <div class="flex space-x-4 animate-scroll inline-block">
+            <div class="flex space-x-5 animate-scroll inline-block">
                 <Image src={"/sponsor1.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor3.png"} alt="Sponsor 1" height={300} width={267} />
                 <Image src={"/sponsor2.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor3.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor4.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor5.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor6.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor7.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor8.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor9.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor10.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor11.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor12.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor13.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor14.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor15.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor16.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor17.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor1.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor2.png"} alt="Sponsor 1" height={300} width={267} />
+                <Image src={"/sponsor3.png"} alt="Sponsor 1" height={300} width={267} />
                 <Image src={"/sponsor4.png"} alt="Sponsor 1" height={300} width={267} />
                 <Image src={"/sponsor5.png"} alt="Sponsor 1" height={300} width={267} />
                 <Image src={"/sponsor6.png"} alt="Sponsor 1" height={300} width={267} />
@@ -22,25 +39,6 @@ export const SponsorsCarousel = () => {
                 <Image src={"/sponsor16.png"} alt="Sponsor 1" height={300} width={267} />
                 <Image src={"/sponsor17.png"} alt="Sponsor 1" height={300} width={267} />
             </div>
-            {/* <div class="flex space-x-4 animate-scroll inline-block">
-                <Image src={"/sponsor1.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor3.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor2.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor4.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor5.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor6.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor7.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor8.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor9.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor10.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor11.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor12.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor13.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor14.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor15.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor16.png"} alt="Sponsor 1" height={300} width={267} />
-                <Image src={"/sponsor17.png"} alt="Sponsor 1" height={300} width={267} />
-            </div> */}
         </div>
     );
 };
