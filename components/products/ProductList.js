aba3406b4208f6f813198bb1c066499f63d220ab
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-08-20 23:12:12 -0300

    categories desde db ok

--- components/products/ProductList.js
+++ components/products/ProductList.js
@@ -1,15 +1,15 @@
 import ProductCard from './ProductCard'
 
 const ProductList = async ({ category }) => {
-    const { data: items } = await fetch(
+    const items = await fetch(
         `http://localhost:3000/api/products/${category}`, {
-        cache: "force-cache",
+        
         next: {
             tags: ["products"],
         }
     }
     ).then(res => res.json())
-
+    
     return (
         <div className="container m-auto flex justify-center items-center gap-12 flex-wrap p-5">
             {items.map(item => <ProductCard key={item.slug} item={item} />)}
