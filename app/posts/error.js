227e4f7d715e7cab5a496157e635ef7fa331abb0
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-09-10 02:10:27 -0300

    add error.js

---
+++ app/posts/error.js
@@ -0,0 +1,18 @@
+'use client'
+import Button from "@/components/Button"
+import { useEffect } from "react"
+
+export default function Error({ error, reset }) {
+
+    useEffect(() => {
+        console.log(error)
+    }, [])
+
+    return (
+        <div className="container m-auto mt-6">
+            <h2>Algo salió mal</h2>
+            <hr />
+            <Button onClick={() => reset()}>Intenta nuevamente</Button>
+        </div>
+    )
+}
