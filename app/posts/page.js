25802b43f7c0c1e9c3dc78dc2318641ae5bc3655
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-09-12 00:21:17 -0300

    finx post page

--- app/posts/page.js
+++ app/posts/page.js
@@ -30,8 +30,8 @@ const Posts = async () => {
             <hr />
             <ul>
                 {posts.map((post) => (
-                    <Link href={`/posts/${post.id}`}>
-                        <li key={post.id} className='my-4 list-disc'>
+                    <Link key={post.id} href={`/posts/${post.id}`}>
+                        <li className='my-4 list-disc'>
                             {post.title}
                         </li>
                     </Link>
